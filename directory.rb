f547a957993ba5ce6cdef6182bc7c8cc0873f59c
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-10-20 16:30:52 +0100

    Added more data within an array of hashes, gave an LUFC flavour to the project

--- directory.rb
+++ directory.rb
@@ -1,42 +1,41 @@
-# Arrange all the students into an array.
+# Arrange all the players into an array.
 
-students = [
-"Darko Milanic",
-"Mirco Antenucci",
-"Marco Silvestri",
-"Giuseppe Bellusci",
-"Gaetano Berardi",
-"Sam Byram",
-"Stephen Warnock",
-"Liam Cooper",
-"Jason Pearce",
-"Rodolph Austin",
-"Tommasso Bianchi",
-"Lewis Cook",
-"Alex Mowatt",
-"Luke Murphy",
-"Nicky Ajose",
-"Billy Sharp",
-"Souleymane Doukara"
+players = [
+	{:name => "Mirco Antenucci", :squad => :season201415}, 
+	{:name => "Marco Silvestri", :squad => :season201415}, 
+	{:name => "Giuseppe Bellusci", :squad => :season201415}, 
+	{:name => "Gaetano Berardi", :squad => :season201415}, 
+	{:name => "Sam Byram", :squad => :season201415}, 
+	{:name => "Stephen Warnock", :squad => :season201415}, 
+	{:name => "Liam Cooper", :squad => :season201415}, 
+	{:name => "Jason Pearce", :squad => :season201415}, 
+	{:name => "Rodolph Austin", :squad => :season201415}, 
+	{:name => "Tommasso Bianchi", :squad => :season201415}, 
+	{:name => "Lewis Cook", :squad => :season201415}, 
+	{:name => "Alex Mowatt", :squad => :season201415}, 
+	{:name => "Luke Murphy", :squad => :season201415}, 
+	{:name => "Nicky Ajose", :squad => :season201415}, 
+	{:name => "Billy Sharp", :squad => :season201415}, 
+	{:name => "Souleymane Doukara", :squad => :season201415}, 
 ]
 
 def print_header
-	puts "The students of my cohort at Makers Academy"
+	puts "The players at Leeds United"
 	puts "-------------------"
 end
 
-def print(names)
-	names.each do |name| 
-		puts name
+def print(players)
+	players.each do |member| 
+		puts "#{member[:name]} (#{member[:squad]} squad}"
 	end
 end
 
 def print_footer(names)
-	puts "Overall we have #{names.length} great students"
+	puts "Overall we have #{names.length} great players"
 end
 
 # nothing happens until we call the methods
 
 print_header
-print(students)
-print_footer(students)
+print(players)
+print_footer(players)
