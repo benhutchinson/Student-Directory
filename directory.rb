703cad4a5dbd06fba3a3fc59c3bae5a3a6896f45
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-10-21 13:13:34 +0100

    Allowed User To Input Cohort Value, with :default cohort symbol defined if nothing submitted

--- directory.rb
+++ directory.rb
@@ -8,20 +8,29 @@ def input_students
 	# get the first name
 	name = gets.chomp
 
-	print "Please enter the country of origin\n"
+	print "What month are you joining Makers?\n"
+	# defining the cohort
+	cohort = gets.chomp
 
+	if cohort.length == 0
+		cohort = :default
+	else
+		cohort.to_sym
+	end
+
+	print "Please enter the country of origin\n"
 	# add the country of origin
 	country = gets.chomp
 
 	print "Any hobbies?\n"
-
-	# add the country of origin
+	# check for hobbies
 	hobbies = gets.chomp
 
 	# while the name is not empty, repeat this code
+
 	while !name.empty? do
 		# add the student hash to the array
-		students << {:name => name, :cohort => :november, :country => country, :hobbies => hobbies}
+		students << {:name => name, :cohort => cohort.to_sym, :country => country, :hobbies => hobbies}
 		print "Now we have #{students.length} students\n"
 		# get another name from the user
 		name = gets.chomp
@@ -32,18 +41,25 @@ end
 
 def print_header
 	puts "The students of my cohort at Makers Academy".center(60, "-")
+	puts ""
 	puts "-------------------".center(60, "/")
 	puts ""
 end
 
 def list(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort from #{student[:country]} who likes #{student[:hobbies]})".center(60, "-")
+    puts "#{student[:name]} (#{student[:cohort]} cohort from #{student[:country]} who likes #{student[:hobbies]})".center(60)
   end
 end
 
 def print_footer(students)
-	print "Overall we have #{students.length} great students\n"
+	puts ""
+	puts "_"*60
+	puts ""
+	print "Overall we have #{students.length} great students\n".center(60)
+	puts ""
+	puts "_"*60
+	puts ""
 end
 
 students = input_students
