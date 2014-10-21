c1a9305a7754cd329edf38726c077724921c2be6
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-10-21 09:36:05 +0100

    Interactive input list of students

--- directory.rb
+++ directory.rb
@@ -1,41 +1,41 @@
-# Arrange all the players into an array.
+def input_students
+	puts "Please enter the name of the students"
+	puts "To finish, just hit return twice"
 
-players = [
-	{:name => "Mirco Antenucci", :squad => :season201415}, 
-	{:name => "Marco Silvestri", :squad => :season201415}, 
-	{:name => "Giuseppe Bellusci", :squad => :season201415}, 
-	{:name => "Gaetano Berardi", :squad => :season201415}, 
-	{:name => "Sam Byram", :squad => :season201415}, 
-	{:name => "Stephen Warnock", :squad => :season201415}, 
-	{:name => "Liam Cooper", :squad => :season201415}, 
-	{:name => "Jason Pearce", :squad => :season201415}, 
-	{:name => "Rodolph Austin", :squad => :season201415}, 
-	{:name => "Tommasso Bianchi", :squad => :season201415}, 
-	{:name => "Lewis Cook", :squad => :season201415}, 
-	{:name => "Alex Mowatt", :squad => :season201415}, 
-	{:name => "Luke Murphy", :squad => :season201415}, 
-	{:name => "Nicky Ajose", :squad => :season201415}, 
-	{:name => "Billy Sharp", :squad => :season201415}, 
-	{:name => "Souleymane Doukara", :squad => :season201415}, 
-]
+	# create an empty array
+	students = []
+
+	# get the first name
+	name = gets.chomp
+
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {:name => name, :cohort => :november}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+	# return the array of students
+	students
+end
 
 def print_header
-	puts "The players at Leeds United"
+	puts "The students of my cohort at Makers Academy"
 	puts "-------------------"
 end
 
-def print(players)
-	players.each do |member| 
-		puts "#{member[:name]} (#{member[:squad]} squad}"
+def print(students)
+	students.each do |student| 
+		puts "#{student[:name]} (#{student[:cohort]} cohort}"
 	end
 end
 
-def print_footer(names)
-	puts "Overall we have #{names.length} great players"
+def print_footer(students)
+	puts "Overall we have #{students.length} great students"
 end
 
-# nothing happens until we call the methods
-
+students = input_students
 print_header
-print(players)
-print_footer(players)
+print(students)
+print_footer(students)
