5baa6001aa26689c07559a8eea48a4e9cf6ca9be
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-10-21 10:01:00 +0100

    Exercise 1: No Puts

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-	puts "Please enter the name of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the name of the students\n"
+	print "To finish, just hit return twice\n"
 
 	# create an empty array
 	students = []
@@ -12,7 +12,7 @@ def input_students
 	while !name.empty? do
 		# add the student hash to the array
 		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		# get another name from the user
 		name = gets.chomp
 	end
@@ -21,21 +21,21 @@ def input_students
 end
 
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-------------------"
+	print "The students of my cohort at Makers Academy\n"
+	print "-------------------\n"
 end
 
-def print(students)
+def list(students)
 	students.each do |student| 
-		puts "#{student[:name]} (#{student[:cohort]} cohort}"
+		print "#{student[:name]} (#{student[:cohort]} cohort}\n"
 	end
 end
 
 def print_footer(students)
-	puts "Overall we have #{students.length} great students"
+	print "Overall we have #{students.length} great students\n"
 end
 
 students = input_students
 print_header
-print(students)
+list(students)
 print_footer(students)
