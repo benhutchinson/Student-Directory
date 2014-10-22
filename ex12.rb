cd1b67a47064cc734555b15f25ec24be2987c9d5
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-10-22 11:54:53 +0100

    Used ARGV to help pre-load the Students.CSV on open

--- ex12.rb
+++ ex12.rb
@@ -19,14 +19,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {:name => name, :cohort => :november}
     puts "Now we have #{@students.length} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -73,7 +73,7 @@ def interactive_menu
   loop do
 
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
 
   end
 end
@@ -91,8 +91,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {:name => name, :cohort => cohort.to_sym}
@@ -100,5 +100,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # does the actual file for filename exist?
+    load_students(filename)
+    puts "Loaded #{@students.length} students from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
 
+try_load_students
 interactive_menu
