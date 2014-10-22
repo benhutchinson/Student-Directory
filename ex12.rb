a9349423a591705b558be3a9f745f7369d860d10
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-10-22 16:05:27 +0100

    Extra exercise: refactors code pushing input into student array into a method called add_student

--- ex12.rb
+++ ex12.rb
@@ -23,7 +23,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {:name => name, :cohort => :november}
+    add_student(name, "november")
     puts "Now we have #{@students.length} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -91,14 +91,21 @@ def save_students
   file.close
 end
 
+
+def add_student(name, cohort)
+  @students << {:name => name, :cohort => cohort.to_sym}
+end
+
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {:name => name, :cohort => cohort.to_sym}
+    add_student(name, cohort)
   end
   file.close
 end
+
+
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
